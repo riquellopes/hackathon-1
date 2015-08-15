acf25d3a236764608c8d3e7737d8b9b78b40a28b
Henrique Lopes <riquellopes@gmail.com>
2015-08-15 18:59:32 -0300

    Adicionando rotas da api

--- config/routes.php
+++ config/routes.php
@@ -3,4 +3,12 @@
 require __DIR__ . "/controllers.php";
 
     $app->get('/', "SiteController:index")->bind('home');
-    $app->get('/api', "Apiontroller:home")->bind('api');
+
+    $app->get('/api', "ApiController:home")->bind('api');
+
+    $app->get('/api/medalhas', "ApiController:medalhas")->bind('api_medalhas');
+    $app->get('/api/missoes/{id}', "ApiController:missoes")->bind('api_missoes');
+    $app->get('/api/carreiras/{id}', "ApiController:missoes")->bind('api_carreiras');
+    $app->get('/api/total-pontos/{id}', "ApiController:total_pontos")->bind('api_total_pontos');
+    $app->get('/api/total-dicas/{id}', "ApiController:total_dicas")->bind('api_total_dicas');
+    $app->get('/api/pontos-proxima/{id}', "ApiController:pontos_proxima")->bind('api_pontos_proxima');
